6c08dde6e9e40f5cc8a597fc0058f6ee445524e6
Axetroy <axetroy.dev@gmail.com>
2024-10-19 11:51:34 +0800

    update

---
+++ lib/checker/isFunctionCall.d.ts
@@ -0,0 +1,23 @@
+import { Node } from 'estree'
+
+interface isFunctionCallOptions {
+    argCount?: number
+    minArgCount?: number
+    maxArgCount?: number
+}
+
+/**
+ * Check if a node is a function call with the given target name
+ * @param node
+ * @param target
+ * @param options
+ * @example
+ * ```js
+ * isFunctionCall(node, 'foo', { argCount: 2 }) // true if node is `foo(1, 2)`
+ * isFunctionCall(node, 'bar.foo()') // true if node is `bar.foo()`
+ * isFunctionCall(node, 'foo', { minArgCount: 2 }) // true if node is `foo(1, 2, 3)`
+ * isFunctionCall(node, 'foo', { maxArgCount: 2 }) // true if node is `foo(1)` or `foo(1, 2)`
+ * isFunctionCall(node, '*.foo', { maxArgCount: 2 }) // true if node is `bar.foo(1, 2)` or `baz.foo(1, 2)`
+ * ```
+ */
+declare function isFunctionCall(node: Node, target: string, options?: isFunctionCallOptions): boolean
