76c90615ddf93d2ad33048d87d6d2b11324e093c
Axetroy <axetroy.dev@gmail.com>
2024-10-23 09:45:44 +0800

    add `isBooleanExpression`

---
+++ lib/checker/boolean.d.ts
@@ -0,0 +1,4 @@
+import { SourceCode } from 'eslint'
+import { Node } from 'estree'
+
+export declare function isBooleanExpression(sourceCode: SourceCode, node: Node): boolean
--- lib/checker/index.d.ts
+++ lib/checker/index.d.ts
@@ -1,3 +1,4 @@
 export { isFunctionCall } from './isFunctionCall'
 export { isInControlFlow } from './isInControlFlow'
 export { isNodeEqual } from './isNodeEqual'
+export { isBooleanExpression } from './boolean'
