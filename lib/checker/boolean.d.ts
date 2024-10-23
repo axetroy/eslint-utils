4ff7b7b286426532aa67753e3c7c42e90906e602
Axetroy <axetroy.dev@gmail.com>
2024-10-23 13:55:43 +0800

    update

--- lib/checker/boolean.d.ts
+++ lib/checker/boolean.d.ts
@@ -9,14 +9,8 @@ import { Node } from 'estree'
 export declare function isBooleanExpression(sourceCode: SourceCode, node: Node): boolean
 
 /**
- * Check if the annotation is a boolean type annotation.
- * @param annotation
- */
-export declare function isBooleanTypeAnnotation(annotation: any): boolean
-
-/**
  * Check if the node is a function return boolean.
  * @param sourceCode
  * @param node
  */
-export declare function isFunctionReturnBoolean(sourceCode: SourceCode, node: Node): boolean
+export declare function isBooleanExpressionFunction(sourceCode: SourceCode, node: Node): boolean
